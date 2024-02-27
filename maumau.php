06a041b386acc51b7f8bf614de9f18ced7ddf770
Mauricio Pereira <mauriciopereira2310@gmail.com>
2024-02-27 13:40:30 -0300

    Feat: Adicionando o comando de init e version

--- maumau.php
+++ maumau.php
@@ -1,25 +1,50 @@
+#!/usr/bin/php
 <?php
 
-$commands  = "";
-$commands .= "c:";
+$RED    = "\033[0;31m"; //VERMELHO
+$GREEN  = "\033[0;32m"; //VERDE
+$YELLOW = "\033[0;33m"; //AMARELO
+$BLUE   = "\033[0;34m"; //AZUL
+$NC     = "\033[0m"; //BRANCO
+
+$short  = "";
+$short .= "c:b:";
 
 // Sem : O paramêtro não aceita dados
 // Com : Obriga a ter valor no paramêtro
 // Com :: Fala que paramêtro é opcional
-$types  = [
+$long  = [
     "clone:",
+    "branch:",
+    "init",
+    "version"
 ];
-$options = getopt($commands, $types);
+$options = getopt($short, $long);
 
-$commands = array_key_first($options);
+$short = array_key_first($options);
 
-switch($commands){
+switch($short){
   case 'c':
   case 'clone':
-    $result = shell_exec("git clone {$options[$commands]}");
-      echo "<pre>"; var_dump($result); echo "</pre>"; exit;
+    shell_exec("git clone {$options[$short]}");
+    echo "{$GREEN}Comando clone executado com sucesso!" . PHP_EOL;
       
     break;
+
+  case 'init':
+    $branch = 'main';
+    if(isset($options['b']) || isset($options['branch'])){
+      $branch = $options['b'] ? $options['b'] : $options['branch'];
+    }
+    
+    shell_exec("git clone --branch {$branch} https://github.com/MauricioCruzPereira/core-maumau-cli.git");
+    echo "{$GREEN}Comando init executado com sucesso!" . PHP_EOL;
+    break;
+
+  case 'version':
+    echo 'v1.0.0' . PHP_EOL;
+    break;
+    
   default:
-    echo 'Comando inválido';
+    echo 'Comando inválido'. PHP_EOL;
 }
